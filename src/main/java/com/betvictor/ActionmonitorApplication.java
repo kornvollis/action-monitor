1852bc451e416aa900a759b9b7ee6381a4f0143e
OMLI <kornvollis@gmail.com>
2015-11-28 10:15:40 +0100

    base project

---
+++ src/main/java/com/betvictor/ActionmonitorApplication.java
@@ -0,0 +1,12 @@
+package com.betvictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionmonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionmonitorApplication.class, args);
+    }
+}
